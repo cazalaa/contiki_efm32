ea319fc4f81089891c5dd564cbeda3e12c3ecfc5
cazalaa <cazalaa@yahoo.fr>
2014-05-26 16:02:00 +0200

    examples

---
+++ examples/efm32/radio-test/radio-test.c
@@ -0,0 +1,40 @@
+/**
+ * \file
+ *         Si446x radio test   TX "hello" payload every second
+ * \author
+ *         Jerome Cloute-Cazalaa
+ */
+
+#include "contiki.h"
+#include "platform-conf.h"
+#include "net/netstack.h"
+#include "dev/leds.h"
+
+#include <stdio.h> /* For printf() */
+
+static struct etimer et_radio;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(hello_world_process, "Radio test process");
+AUTOSTART_PROCESSES(&radio_test_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(radio_test_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  printf("\n\rTest Radio\n\r");
+  
+  while(1) {
+    etimer_set(&et_radio, CLOCK_SECOND );
+    PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+    NETSTACK_RADIO.send("hello",5);
+    leds_toggle(LEDS_ALL);
+    etimer_set(&et_radio, CLOCK_SECOND );
+    PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+    si446x_change_state(1);
+   
+  }
+  
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
